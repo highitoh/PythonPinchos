66914f11b2710be7d94f4af14c3b77027f901eb8
highitoh <highitoh@gmail.com>
2019-10-05 11:54:39 +0900

    [doxygen] Remove a glob logic from the member comment analyzer

--- doxygen/member_comment_analyzer.py
+++ doxygen/member_comment_analyzer.py
@@ -1,33 +1,39 @@
 import pandas as pd
 import xml.etree.ElementTree as ET
-import glob
 import re
 import os
 
 class MemberCommentAnalyzer:
+    """
+    Analyzer of comments which are described in the members of classes.
+    It analyzes XML format files that Doxygen outputs.
+    """
 
-    def __init__(self, path):
-        self.path = path
+    COMPOUND_NAME_COLUMN_NAME = 'compound_name'
+    MEMBER_NAME_COLUMN_NAME = 'member_name'
+    COMMENT_COLUMN_NAME = 'comment'
+    TEXT_DF_COLUMNS = [COMPOUND_NAME_COLUMN_NAME, MEMBER_NAME_COLUMN_NAME, COMMENT_COLUMN_NAME]
+
+    def __init__(self, filename_list):
+        """
+        Constructor
+
+        Parameters
+        ----------
+        filename_list : list of str
+            The list of The names of XML files to be analyzed.
+        """
+        self.result = {}
+        self.filename_list = filename_list
 
     def analyze(self):
-        xml_file_names = glob.glob(self.path)
-        self.result = {}
-
-        for xml_file_name in xml_file_names:
-            tree = ET.parse(xml_file_name)
+        """
+        Analyze comments from the XML files specified in the filename list.
+        """
+        for filename in self.filename_list:
+            tree = ET.parse(filename)
             compounddefs = tree.findall('./compounddef')
             self.result.update(self.__get_compounddefs_comment_dict(compounddefs))
-
-    def get_dataframe(self):
-        df = pd.DataFrame(columns=['compound_name', 'member_name', 'comment'])
-
-        for compound_name in self.result.keys():
-            member_comment_dict = self.result[compound_name]
-            for member_name in member_comment_dict:
-                comment = member_comment_dict[member_name]
-                s = pd.Series([compound_name, member_name, comment], index=df.columns)
-                df = df.append(s, ignore_index=True)
-        return df
 
     def __get_compounddefs_comment_dict(self, compounddefs):
         compound_member_comment_dict = {}
@@ -50,4 +56,21 @@ class MemberCommentAnalyzer:
             member_comment_dict[member_name] = para_text
         return member_comment_dict
 
+    def get_dataframe(self):
+        """
+        Get analyzed comments by a dataframe.
 
+        returns
+        -------
+        DataFrame
+            A dataframe which contains analyzed comments.
+        """
+        df = pd.DataFrame(columns=self.TEXT_DF_COLUMNS)
+
+        for compound_name in self.result.keys():
+            member_comment_dict = self.result[compound_name]
+            for member_name in member_comment_dict:
+                comment = member_comment_dict[member_name]
+                s = pd.Series([compound_name, member_name, comment], index=df.columns)
+                df = df.append(s, ignore_index=True)
+        return df
